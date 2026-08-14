524007b529732a9f95674bfbc835566612f87b79
Justxd22 <POM@MacBook-Pro-von-User.local>
2026-08-14 20:44:06 +0300

    Confirm Supabase Storage RLS integration and live file upload with public URLs

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "504666137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3604055449" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
